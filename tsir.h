6d2ea912a190752fe7135ec06c24f65218919b99
Petter Holme <pttrhlm@gmail.com>
2018-07-27 10:33:45 +0900

    adapted to the new RNG

--- tsir.h
+++ tsir.h
@@ -3,24 +3,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <math.h>
-#include <float.h>
 #include <time.h>
 #include <limits.h>
-#include "../SFMT/SFMT.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdint.h>
 
 #define NAVG 100000 // number of runs for averages
-#define NRND 0x10000 // number of random numbers per batch
-#define NREV 1000 // number of iterations to thermalize the RNG
 
 #define NONE (UINT_MAX - 1)
 #define END UINT_MAX // NONE and END are used in various ways, the only purpose of NONE < END is for the S(x) macro
 
 #define S(x) (n[(x)].heap < END) // is x susceptible?
-
-// if rng needs a refill within x new random numbers
-#define RND_CHK(x) if ((x) + (g.r - g.rnd) >= NRND) {sfmt_fill_array32(&g.sfmt, g.rnd, NRND); g.r = g.rnd;}
 
 // auxiliary macro
 #define SQ(x) ((x) * (x))
@@ -28,28 +23,22 @@
 typedef struct GLOBALS {
 	// INPUT PARAMETERS
 	double recovery_scale; // recovery time scale, auxiliary value for infection probs
-	unsigned short prob[0x10001];
+	unsigned short rnd2inx[0x10001]; // mapping 16-bit random number to index
 	// NETWORK SPECS
 	unsigned int n, dur;
 	// OTHER GLOBALS
-	unsigned int nheap, *heap, *order, *dstream; // infection order
+	unsigned int nheap, *heap;
+	// FOR RND
+	uint32_t state;
+	unsigned int cutoff_source, cutoff_dur; // to get the probabilities right . .
 	// OUTBREAK STATS
 	unsigned int s;
-	// FOR RNG
-	uint32_t *rnd, *r; // arrays for random numbers
-	sfmt_t sfmt; // state of random number generator
-	unsigned int cutoff_source, cutoff_dur; // to get the probabilities right . .
 } GLOBALS;
 
 typedef struct NODE {
 	unsigned int deg, *nb; // degree, neighbors
 	unsigned int *nc, **t; // ordered number of / list of contact times for bisection serach
-	unsigned int heap, itime, rtime; // time is 1st the time of infection (for sorting the heap), then the time of recovery (to check if the node is I or R)
-	unsigned int parent;
-	// for stats
-	double dstream, dstream0;
-	unsigned int ninfect;
-	double tinfect;
+	unsigned int heap, time; // time is 1st the time of infection (for sorting the heap), then the time of recovery (to check if the node is I or R)
 } NODE;
 
 // heap.c
@@ -60,10 +49,14 @@ extern void del_root ();
 extern void init_rng ();
 extern void read_data (FILE *);
 extern unsigned int exptime ();
-extern unsigned int get_source ();
-extern unsigned int get_start_time ();
 
 // quick.c
 extern void quick (unsigned int);
+
+// pcg_rnd.c
+extern uint16_t pcg_16 ();
+extern uint32_t pcg_32 ();
+extern uint32_t pcg_32_bounded ();
+extern void pcg_init ();
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
