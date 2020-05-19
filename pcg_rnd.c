f5bf4d2ceb2edce1127856045c058988288a089e
Petter Holme <pttrhlm@gmail.com>
2020-05-20 00:17:06 +0900

    bug fix

--- pcg_rnd.c
+++ pcg_rnd.c
@@ -13,34 +13,6 @@ extern GLOBALS g;
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-void pcg_init () {
-	int fd;
-	unsigned long sz;
-	uint64_t initstate;
-
-	if (g.state != 0) return;
-
-	fd = open("/dev/random", O_RDONLY);
-	
-	if (fd < 1) {
-		fprintf(stderr, " can't open /dev/random\n");
-		exit(1);
-	}
-
-	sz = read(fd, &initstate, sizeof(uint64_t));
-	
-	close(fd);
-
-	if (sz < sizeof(uint64_t)) {
-		fprintf(stderr, " can't read /dev/random\n");
-		exit(1);
-	}
-
-	g.state = (initstate + 1442695040888963407ULL) * 6364136223846793005ULL + 1442695040888963407ULL;
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 uint32_t pcg_32 () {
 	uint64_t state = g.state;
 	uint32_t value, rot;
@@ -69,11 +41,11 @@ uint16_t pcg_16 () {
 
 	if (exist) {
 		exist = 0;
-		return (uint16_t) g.rmem >> 16;
+		return g.rmem >> 16;
 	}
 	exist = 1;
 	g.rmem = pcg_32();
-	return (uint16_t) g.rmem;
+	return g.rmem;
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
